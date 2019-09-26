4535fbbafd9f6cf469410f736aa320b40f2f7de2
Andrew Capp <acapp909@gmail.com>
2019-09-26 17:06:36 +0000

    Automatically backed up by Learn

--- toon.rb
+++ toon.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array) # code an argument here
  
   i=0 
  while i < cheese_types.length do 
-    if array.include? (cheese_types[i]) == true 
+    if array.include? ("#{cheese_types[i]}") == true 
       p cheese_types[i]
       return cheese_types[i]
     end
