fea29ff0f97c11e2b9798e3e462654dccd5a58d3
Andrew Capp <acapp909@gmail.com>
2019-09-26 17:11:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,16 @@ def long_planeteer_calls(array) # code an argument here
   array.any? {|word| word.length>4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+ 
+ i=0 
+ while i < cheese_types.length do 
+    if array.include? (cheese_types[i]) 
+      return cheese_types[i]
+    end
+    i +=1 
+  end
+  return nil 
+    
