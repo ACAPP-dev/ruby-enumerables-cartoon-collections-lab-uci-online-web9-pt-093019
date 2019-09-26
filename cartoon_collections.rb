1b56ca513f2cbbc3a8d43fecaebb42d3b3dc6d05
Andrew Capp <acapp909@gmail.com>
2019-09-26 16:56:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+   array.each_with_index {|elem, ind| puts "#{ind+1} #{elem}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  array.map {|item| "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+  array.any? {|word| word.length>4}
 end
 
 def find_the_cheese# code an argument here
